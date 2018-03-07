f01fa233216507937456e016d84475a7df337c27
jdxlabs <jdevoucoux@gmail.com>
2018-03-07 14:59:59 +0100

    init terraform workshop

---
+++ Workshop1-Terraform/input.tf
@@ -0,0 +1,18 @@
+
+variable "initials" {}
+
+variable "project_name" {
+    default = "sandbox"
+}
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "zones" {
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
