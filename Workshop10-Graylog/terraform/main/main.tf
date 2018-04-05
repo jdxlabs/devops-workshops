d15ea156618ec5a3ed93f6d72479f24049b53e1e
jdxlabs <jdevoucoux@gmail.com>
2018-04-05 18:02:24 +0200

    ws10 work

--- Workshop10-Graylog/terraform/main/main.tf
+++ Workshop10-Graylog/terraform/main/main.tf
@@ -130,14 +130,14 @@ resource "aws_security_group" "logstore_ingress" {
     from_port   = 80
     to_port     = 80
     protocol    = "TCP"
-    cidr_blocks = ["${var.bastion_ingress_cidr}"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 9000
     to_port     = 9000
     protocol    = "TCP"
-    cidr_blocks = ["${var.bastion_ingress_cidr}"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
